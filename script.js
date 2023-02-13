f25e11eb76583178d7bc737f16a23cab5aac7c61
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-02-13 14:20:43 -0300

    Versão melhorada com SwitchAlert2 e Bootstrap

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
-const cells = document.querySelectorAll("td");
-let turn = "X";
 
-for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener("click", function () {
-    if (this.textContent === "") {
-      this.textContent = turn;
-      checkWin();
-      checkDraw();
-      changeTurn();
-    }
-  });
-}
+  let currentPlayer = "X";
+  let cells = document.getElementsByClassName("cell");
 
-function changeTurn() {
-  turn = turn === "X" ? "O" : "X";
-}
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", function(event) {
+      if (event.target.innerHTML === "") {
+        event.target.innerHTML = currentPlayer;
+        checkWinner();
+        switchPlayer();
+      }
+    });
+  }
 
-function checkWin() {
-  const combinations = [    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < combinations.length; i++) {
-    const [a, b, c] = combinations[i];
-    if (
-      cells[a].textContent === turn &&
-      cells[b].textContent === turn &&
-      cells[c].textContent === turn
-    ) {
-      alert(`${turn} ganhou!`);
-      resetBoard();
+  function switchPlayer() {
+    if (currentPlayer === "X") {
+      currentPlayer = "O";
+    } else {
+      currentPlayer = "X";
     }
   }
-}
 
-function checkDraw() {
-  for (let i = 0; i < cells.length; i++) {
-    if (cells[i].textContent === "") {
-      return;
+  function checkWinner() {
+    let winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+      let combination = winningCombinations[i];
+      if (
+        cells[combination[0]].innerHTML === currentPlayer &&
+        cells[combination[1]].innerHTML === currentPlayer &&
+        cells[combination[2]].innerHTML === currentPlayer
+      ) {
+        Swal.fire({
+          title: `Jogador ${currentPlayer} ganhou!`,
+          text: "Clique em OK para jogar novamente",
+          icon: "success",
+          confirmButtonText: "OK"
+        }).then(() => {
+          for (let i = 0; i < cells.length; i++) {
+            cells[i].innerHTML = "";
+          }
+        });
+        return;
+      }
+    }
+
+    let draw = true;
+    for (let i = 0; i < cells.length; i++) {
+      if (cells[i].innerHTML === "") {
+        draw = false;
+        break;
+      }
+    }
+
+    if (draw) {
+      Swal.fire({
+        title: "Empate!",
+        text: "Clique em OK para jogar novamente",
+        icon: "warning",
+        confirmButtonText: "OK"
+      }).then(() => {
+        for (let i = 0; i < cells.length; i++) {
+          cells[i].innerHTML = "";
+        }
+      });
     }
   }
-  alert("Empate!");
-  resetBoard();
-}
-
-function resetBoard() {
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].textContent = "";
-}
-turn = "X";
-}
-
